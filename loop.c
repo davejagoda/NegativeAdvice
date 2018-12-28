841117c5a834c651628e30482a0ba44006813df7
dj <davejagoda@users.noreply.github.com>
2018-12-28 14:37:07 +0000

    Add details about max and min to loop.c

--- loop.c
+++ loop.c
@@ -6,11 +6,16 @@
 
 int main() {
   int i = 1;
+  int max = 1;
+  int min = 1;
   while (i) {
 #ifdef DEBUG
     if (i % (512 * 1024 * 1024) == 0) printf("i: %12d\n", i);
 #endif
     ++i;
+    if (i < min) min = i;
+    if (i > max) max = i;
   }
   printf("infinite loop ended\n");
+  printf("max: %d min: %d\n", max, min);
 }
